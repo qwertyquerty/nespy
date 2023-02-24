ea8ad67cd45b2254f2f04afe570674a5dc284599
napip03 <ford.nhc@gmail.com>
2023-02-24 13:35:27 -0800

    Update NES6052.py

--- NES6052.py
+++ NES6052.py
@@ -2,15 +2,21 @@
 #theoretically 2kb of memory with storage being accessed from the game card.
 #64 sprites per screen
 #25 / 52 colors
-# 256 by 240 screen size
+# 256 by 224 screen size
 #8x8 tile background
+# display output SDL2
+
 import math
 import functools
+
+
+# find ways to change if statements to conditionals
+
 
 #2^16 memory addresses 
 #(PAGE ZERO/1) first 256 bytes act as a register
     #program counter - pointer to the address that the CPU is executing the code for
-class Registers: 
+class Registers: #subclass to CPU
     #cpu register object
     def __init__(self, pc) -> None:
         self.reset(pc)
@@ -22,19 +28,18 @@ class Registers:
         self.pc = pc #program counter
 #(PAGE 2 ) second 256 bytes act as STACK memory
         
-
-class Memory:
+class Memory: 
     def __init__(self, size: int =  65536) -> None:        
         if ( 0< size - 1 ):
             raise ValueError("Memory size is not available")
         self.size = size;
         self.memory = [0] * self.size;
-    def __getItem__(self, address: int) -> int: #get item @ address requires memory and address
+    def getItem(self, address: int) -> int: #get item @ address requires memory and address
         if 0x0000 < address > self.size:
             raise ValueError("Memory Address is not valid or OoB") # avoid returning data that is not ours
         return self.memory[address]
-    def __setItem__(self , address : int , value: int ) -> int:#not none for good sty. 
-        if 0x0000 < address > self.size:
+    def setItem(self , address : int , value: int ) -> int:#not none for good sty. 
+        if 0x0000 < address >= self.size:
             raise ValueError("Memeory Address is not valid")
         if value.bit_length() > 8:
             raise ValueError("Value trying to be written is too large")
@@ -42,9 +47,8 @@ class Memory:
         return self.memory[address]
 
 class Cpu:
-    def __init__(self, memory: None, pc = 0x0000) -> None: #other nessary mem location
+    def __init__(self, pc = 0x0000) -> None: #other nessary mem location
         self.name =  "6502"
-        self.memory = memory
         self.start_pc = pc #i dont think this will cause errors with the PC in class Register 
         #VM shit
         self.excycles = 0 
@@ -52,11 +56,12 @@ class Cpu:
         self.processorCycles = 0
         #setting up registry
         self.registers = Registers(self)
-        if memory is None:
-            self.memory = Memory()
-            self.start_pc = pc #might be redundant
+        self.memory = Memory()
         self.reset()
     def reset(self):
         self.registers = Registers(self.start_pc)
-        
+    def ZeroPageAddr(self):
+        return self.memory.getItem(0)
+    
+    
     
