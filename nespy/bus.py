d8fa855f2da51e0d356e52c059c2fbc3ceeec3f9
madeline <qwertytrogi@gmail.com>
2023-03-06 01:02:57 -0800

    cart, rom loading, and mapper

--- nespy/bus.py
+++ nespy/bus.py
@@ -5,13 +5,3 @@ class Bus():
     def __init__(self):
         self.cpu = Cmp6502(self)
         self.ram = CmpRAM(self)
-
-    def read(self, addr: int, read_only: bool = False) -> int:
-        return self.ram.read(addr)
-
-    def write(self, addr: int, value: int) -> None:
-        # ensure the addr and value are in accepted ranges for byte widths
-        addr &= 0xFFFF
-        value &= 0xFF
-        
-        self.ram.write(addr, value)
---
+++ nespy/mapper.py
@@ -0,0 +1,46 @@
+from typing import Tuple
+
+MIRROR_HARDWARE = 1
+MIRROR_HORIZONTAL = 2
+MIRROR_VERTICAL = 3
+MIRROR_ONSCREEN_LO = 4
+MIRROR_ONSCREEN_HI = 5
+
+class Mapper():
+    prg_banks: int = 0
+    chr_banks: int = 0
+
+    def __init__(self, prg_banks: int, chr_banks: int):
+        self.prg_banks = prg_banks
+        self.chr_banks = chr_banks
+
+    def map_cpu_read(self, addr: int) -> Tuple[int, int]: # (addr, value)
+        pass
+
+    def map_cpu_write(self, addr: int, value: int) -> int: # addr
+        pass
+
+    def map_ppu_read(self, addr: int) -> int: # addr
+        pass
+
+    def map_ppu_write(self, addr: int) -> int: # addr
+        pass
+
+    def reset(self):
+        pass
+
+    def mirror_mode(self) -> int:
+        pass
+
+    def irq_state(self) -> bool:
+        pass
+
+    def irq_clear(self):
+        pass
+
+    def scanline(self):
+        pass
+
+MAPPER_LOOKUP = {
+
+}
